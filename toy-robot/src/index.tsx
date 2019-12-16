4aee7b818602186658295b79b313b390b3ad8a0f
Hans Jakob Emmel <hans.emmel@gmail.com>
2019-12-16 23:02:28 +1300

    remove folder

--- toy-robot/src/index.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom";
-
-ReactDOM.render(
-    <>
-        <div>First App</div>
-    </>,
-    document.getElementById("app") as HTMLElement
-);
